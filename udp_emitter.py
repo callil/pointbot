cfe45492c5396cdf05a9f03a0c3d090016653f39
Callil Capuozzo <callil.capuozzo@gmail.com>
2018-06-18 22:39:25 -0400

    udp emmiter sending two devices

--- udp_emitter.py
+++ udp_emitter.py
@@ -9,7 +9,6 @@ import os
 
 
 TRACKER_BROADCAST_PORT = 8053
-
 DESKTOP_IP          = '192.168.1.133'
 DESKTOP_LISTEN_PORT = 8059
 
@@ -36,7 +35,33 @@ class Server(asyncore.dispatcher_with_send):
             sent = self.sendto(self.buffer,('192.168.1.154',8054)) ## RECEIVING ON PORT 8054
             self.buffer = self.buffer[sent:]
 
-
+def from_controller_state_to_dict(pControllerState):
+    # docs: https://github.com/ValveSoftware/openvr/wiki/IVRSystem::GetControllerState
+    d = {}
+    d['unPacketNum'] = pControllerState.unPacketNum
+    # on trigger .y is always 0.0 says the docs
+    d['trigger'] = pControllerState.rAxis[1].x
+    # 0.0 on trigger is fully released
+    # -1.0 to 1.0 on joystick and trackpads
+    d['trackpad_x'] = pControllerState.rAxis[0].x
+    d['trackpad_y'] = pControllerState.rAxis[0].y
+    # These are published and always 0.0
+    # for i in range(2, 5):
+    #     d['unknowns_' + str(i) + '_x'] = pControllerState.rAxis[i].x
+    #     d['unknowns_' + str(i) + '_y'] = pControllerState.rAxis[i].y
+    d['ulButtonPressed'] = pControllerState.ulButtonPressed
+    d['ulButtonTouched'] = pControllerState.ulButtonTouched
+    # To make easier to understand what is going on
+    # Second bit marks menu button
+    d['menu_button'] = bool(pControllerState.ulButtonPressed >> 1 & 1)
+    # 32 bit marks trackpad
+    d['trackpad_pressed'] = bool(pControllerState.ulButtonPressed >> 32 & 1)
+    d['trackpad_touched'] = bool(pControllerState.ulButtonTouched >> 32 & 1)
+    # third bit marks grip button
+    d['grip_button'] = bool(pControllerState.ulButtonPressed >> 2 & 1)
+    # System button can't be read, if you press it
+    # the controllers stop reporting
+    return d
 
 def main(): 
     loop_thread = threading.Thread(target=asyncore.loop, name="Asyncore Loop", daemon=True)
@@ -64,21 +89,44 @@ def main():
         print("Invalid number of arguments")
         interval = False
         
+
+    
+
     if interval:
         print("hi") 
         while(True):
             start = time.time()
             txt = ""
-            data = v.devices["tracker_1"].get_pose_quaternion()
-            # byteData = bytes(data, "utf-8")
-            strData = ','.join(str(i) for i in data)
-            # strData = str(data[4])
-            # strData =  strData[:strData.find(".")]
-            print(strData)
+            print(v.devices.keys())
+
+
+            strData = ""
+
+            devicename = "tracker_1"
+            if(devicename in v.devices):
+                trackerData = v.devices[devicename].get_pose_euler()
+                strData += devicename + "_POSE " + ','.join(str(i) for i in trackerData) + "|"
             
+            devicename = "controller_1"
+            if(devicename in v.devices):
+            #     print ("==========")
+            #     print(v.devices['controller_1'].vr.getControllerState(0)[1])
+            #     print(from_controller_state_to_dict(v.devices['controller_1'].vr.getControllerState(0)[1]))
+                trackerData = v.devices[devicename].get_pose_euler()
+                strData += devicename + "_POSE " + ','.join(str(i) for i in trackerData)
+            #     #v.vr.triggerHapticPulse(handset.index,0,pulse)
+            #     res, controllerState = v.vr.getControllerState(0)
+            #     for i in controllerState.rAxis:
+            #         print(i.x, i.y)
+            #     print("controllerstate =>")
+            #     print(from_controller_state_to_dict(controllerState))
+            #     print(controllerState.ulButtonPressed)
+            #     print(controllerState.ulButtonTouched)
+            
+            print(strData)
             byteData = bytes(strData, "utf-8")
             cs.sendto(byteData , server_address)
-            
+
 
             print("\r" + txt, end="")
             sleep_time = interval-(time.time()-start)
